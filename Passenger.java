62c1f64c3cf86e77f51d5d2cebd3510a6b6165af
Earl Angelo A. Aguilar <earl_aguilar@dlsu.edu.ph>
2022-06-12 20:54:46 +0800

    Passenger class rebranded

--- Passenger.java
+++ Passenger.java
@@ -1,20 +1,21 @@
-public class Passenger implements Runnable {
+import java.math.*;
+class Passenger implements Runnable {
     private int id;
-    private Monitor passengerMon;
+    private Monitor mtrPSNGR;
 
-    public Passenger(int i, Monitor monitorIn) {
-        id = i;
-        this.passengerMon = monitorIn;
+    public Passenger(int num, Monitor mtr) {
+        id = num;
+        this.mtrPSNGR = mtr;
     }
 
     public void run() {
         // while(true) {
         for (int i = 0; i < 1; i++) { // every passenger only takes one ride
             try {
-                Thread.sleep((int) (Math.random() * 2000));
+                Thread.sleep((int) (Math.random() * 4000));
             } catch (InterruptedException e) {
             }
-            passengerMon.tryToGetOnCar(id);
+            mtrPSNGR.tryToGetOnCar(id);
         }
     }
 }
